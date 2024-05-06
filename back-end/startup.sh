aee43d2dab61aa0751880e0456d699ea81f5ec4c
LakshitaBh <lakshitabhargava96@gmail.com>
2024-05-06 01:12:35 -0500

    Debugging

--- back-end/startup.sh
+++ back-end/startup.sh
@@ -6,6 +6,8 @@ cd /usr/src/app
 # Copy .env file from Cloud Storage to the current directory
 gsutil cp gs://eduhub-sand-bkt/.env .
 
+ls -al
+
 echo "Starting backend server..."
 
 # Run your Python script
